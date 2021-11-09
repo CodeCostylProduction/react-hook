1e0ac61ac4e590c9fb78396fa15c1e7d592090e7
CodeCostylProduction <starostin039@gmail.com>
2021-11-09 15:15:38 +0300

    up to 113 ex

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/navbar'
 import Home from "./pages/home";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from "./pages/about";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-      <>
+      <BrowserRouter>
           <Navbar/>
           <div className='container-fluid pt-4'>
-              <Home/>
+              <Routes>
+                  <Route path='/' exact element={<Home/>}/>
+                  <Route path='/about' element={<About/>}/>
+                  <Route path='/profile/:name' element={<Profile/>}/>
+              </Routes>
           </div>
-          </>
+          </BrowserRouter>
   )
 }
 

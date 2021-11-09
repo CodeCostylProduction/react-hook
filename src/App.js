8b9af3ae6056c5bf2bf097a88ea7b6c8fd28dfdd
CodeCostylProduction <starostin039@gmail.com>
2021-11-09 14:23:41 +0300

    up to 112 ex

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Navbar from './components/navbar'
+import Home from "./pages/home";
 
 function App() {
   return (
-      <div className='container-fluid pt-4'>
-        <h1>Hello world</h1>
-      </div>
+      <>
+          <Navbar/>
+          <div className='container-fluid pt-4'>
+              <Home/>
+          </div>
+          </>
   )
 }
 

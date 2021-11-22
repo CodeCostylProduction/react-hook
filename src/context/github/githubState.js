2447c36eef325e5c989d514621324c102a0369b0
CodeCostylProduction <starostin039@gmail.com>
2021-11-22 20:22:21 +0300

    up to 120 ex

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -1,7 +1,13 @@
 import React, {useReducer} from 'react'
 import {GithubContext} from "./githubContext";
 import {githubReducer} from "./githubReducer";
+import axios from "axios";
+
 import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../types";
+
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
 
 export const GithubState = ({children}) => {
     const initialState = {
@@ -15,12 +21,17 @@ export const GithubState = ({children}) => {
 
     const search = async value => {
         setLoading()
-        // ...
+
+        const response = await axios.get(
+            `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+        )
 
         dispatch({
             type: SEARCH_USERS,
-            payload: []
+            payload: response.data.items
         })
+
+        console.log(response.data)
     }
 
     const getUser = async name => {

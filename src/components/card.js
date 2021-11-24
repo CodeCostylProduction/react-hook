e1badf7dce71b0550921818118652c13bd770d5e
CodeCostylProduction <starostin039@gmail.com>
2021-11-24 11:28:53 +0300

    up to 122 ex

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-const Card = () => {
+const Card = ({user}) => {
     return (
         <div className="card">
-            <img src={""} alt={""} className="card-img-top"/>
+            <img src={user.avatar_url} alt={user.login} className="card-img-top"/>
             <div className="card-body">
-                <h5 className="card-body">React JS</h5>
-                <Link to={'/profile/' + 'react'} className="btn btn-primary">Открыть</Link>
+                <h5 className="card-body">{user.login}</h5>
+                <Link to={'/profile/' + user.id} className="btn btn-primary">Открыть</Link>
             </div>
         </div>
     );
